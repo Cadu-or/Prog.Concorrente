72537bbb8fbfa13f919100553e3ec1941b1d8630
Carlos Eduardo <52487636+Cadu-or@users.noreply.github.com>
2021-05-31 12:33:23 -0300

    Estudo dirigido 7.

---
+++ barbeiro.c
@@ -0,0 +1,77 @@
+/*
+	Carlos Eduardo de Oliveira Ribeiro - 180099094.
+	Estudo dirigido 7 - Problema do barbeiro dorminhoco.
+*/
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define N_CLIENTES 50
+#define N_CADEIRAS 5
+
+sem_t c_barbeiro;
+sem_t c_cliente;
+sem_t cortando;
+sem_t sem_cadeiras;
+
+void * f_barbeiro(void *v){
+	while(1){
+		sem_wait(&c_cliente);		// Esperar/dormindo algum cliente sentar na cadeira do barbeiro (e acordar o barbeiro).
+		sleep(1); 					// Cortar o cabelo do cliente 
+		printf("Barbeiro cortou o cabelo de um cliente\n");
+		sem_post(&cortando);		// Liberar/desbloquear o cliente
+	}
+
+	pthread_exit(0);
+}
+
+void* f_cliente(void* v) {
+	int id = *(int*) v;
+	sleep(id%3);
+	if(sem_trywait(&sem_cadeiras) == 0){ // Conseguiu pegar uma cadeira de espera
+		printf("Cliente %d entrou na barbearia \n",id);
+
+		sem_wait(&c_barbeiro);				// Pegar/sentar a cadeira do barbeiro
+		
+		printf("Cliente %d sentou para cortar o cabelo \n", id);
+
+		sem_post(&sem_cadeiras);			// Liberar a sua cadeira de espera
+		sem_post(&c_cliente);				// Acordar o barbeiro para cortar seu cabelo
+		sem_wait(&cortando);				// Aguardar o corte do seu cabelo
+		sem_post(&c_barbeiro);				// Liberar a cadeira do barbeiro
+
+		printf("Cliente %d cortou o cabelo e foi embora \n",id);
+
+	}else{									// Barbearia cheia
+		printf("Barbearia cheia, cliente %d indo embora \n",id);
+	}
+
+	pthread_exit(0);
+}
+
+int main() {
+	pthread_t thr_clientes[N_CLIENTES], thr_barbeiro;
+	int i, id[N_CLIENTES];
+
+	sem_init(&sem_cadeiras, 0, N_CADEIRAS);
+	sem_init(&c_barbeiro, 0, 1);
+	sem_init(&c_cliente, 0, 0);
+	sem_init(&cortando, 0, 0);
+
+	for (i = 0; i < N_CLIENTES; i++) {
+		id[i] = i;
+		pthread_create(&thr_clientes[i], NULL, f_cliente, (void*) &id[i]);
+	}
+
+	pthread_create(&thr_barbeiro, NULL, f_barbeiro, NULL);
+
+	for (i = 0; i < N_CLIENTES; i++) 
+		pthread_join(thr_clientes[i], NULL);
+	
+	/* Barbeiro assassinado */
+  
+	return 0;
+}
