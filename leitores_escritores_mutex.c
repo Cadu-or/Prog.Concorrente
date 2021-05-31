ebfd5ecdf56afb3520fd6a376a94d46cf4934b60
Carlos Eduardo <52487636+Cadu-or@users.noreply.github.com>
2021-05-31 12:26:55 -0300

    Estudo dirigido 2.

---
+++ leitores_escritores_mutex.c
@@ -0,0 +1,113 @@
+/*
+	Carlos Eduardo de Oliveira Ribeiro - 180099094
+	Estudo dirigido 2 - Problema dos leitores e escritores
+*/
+
+#include "stdio.h"
+#include "unistd.h"
+#include "stdlib.h"
+#include "pthread.h"
+
+#define TRUE 1
+
+#define NE 3 //numero de escritores
+#define NL 10 //numero de leitores
+
+pthread_mutex_t lock_bd = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock_nl = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock_ne = PTHREAD_MUTEX_INITIALIZER;
+
+int num_leitores = 0;
+int num_escritores = 0;
+
+void * reader(void *arg);
+void * writer(void *arg);
+void read_data_base();
+void use_data_read();
+void think_up_data();
+void write_data_base();
+
+int main(){
+	pthread_t r[NL], w[NE];
+	int i;
+    int *id;
+    /* criando leitores */
+    for(i = 0; i < NL ; i++){
+        id = (int *) malloc(sizeof(int));
+        *id = i;
+	    pthread_create(&r[i], NULL, reader, (void *) (id));
+	}
+	/* criando escritores */
+	for (i = 0; i< NE; i++) {
+	    id = (int *) malloc(sizeof(int));
+        *id = i;
+		pthread_create(&w[i], NULL, writer, (void *) (id));
+	}
+	pthread_join(r[0],NULL);
+
+	return 0;
+}
+
+void * reader(void *arg){
+    int i = *((int *) arg);
+    while(TRUE){                      			/* repete para sempre */
+
+		pthread_mutex_lock(&lock_ne);		/* Se existe escritor na posse do turno ele nao roda */
+			pthread_mutex_lock(&lock_nl);
+				num_leitores++;
+				if(num_leitores == 1){
+					pthread_mutex_lock(&lock_bd);
+				}
+			pthread_mutex_unlock(&lock_nl);
+		pthread_mutex_unlock(&lock_ne);
+
+    	read_data_base(i);             						/* acesso aos dados */
+		 
+    	pthread_mutex_lock(&lock_nl);
+			num_leitores--;
+	    	if(num_leitores == 0){ 
+		    	pthread_mutex_unlock(&lock_bd);
+		    }
+        pthread_mutex_unlock(&lock_nl);
+
+    	use_data_read(i);           						/* região não crítica */
+		
+	}
+    pthread_exit(0);
+}
+
+void * writer(void *arg) {
+	int i = *((int *) arg);
+	while(TRUE){                          	/* Repete para sempre */
+	    think_up_data(i);                 	/* Região não crítica */
+		pthread_mutex_lock(&lock_ne);		/* Trava pois existe leitores querendo escrever */
+            pthread_mutex_lock(&lock_bd);
+            	write_data_base(i);       	/* Atualiza os dados */
+				num_escritores--;			/* Decrementa por ja ter usado um escritor */
+	    	pthread_mutex_unlock(&lock_bd);
+
+		pthread_mutex_unlock(&lock_ne);		/* Libera para os leitores conseguirem fazer suas funcoes */
+    }
+
+    pthread_exit(0);
+}
+
+void read_data_base(int i) {
+	printf("Leitor %d está lendo os dados! Número de leitores: %d\n", i,num_leitores);
+	sleep(rand() % 5);
+}
+
+void use_data_read(int i) {
+	printf("Leitor %d está usando os dados lidos! Número de leitores: %d\n", i,num_leitores);
+	sleep(rand() % 5);
+}
+
+void think_up_data(int i) {
+	printf("Escritor %d está pensando no que escrever!\n", i);
+	sleep(rand() % 5);
+}
+
+void write_data_base(int i) {
+	printf("Escritor %d está escrevendo os dados! Número de leitores: %d\n", i,num_leitores);
+	sleep( rand() % 5 + 15);
+}
